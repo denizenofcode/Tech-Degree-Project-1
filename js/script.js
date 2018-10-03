a31774c134084b72e4cf7f37901b48dd3d3159ef
denizenofcode <austin.buie@gmail.com>
2018-10-03 16:29:19 -0400

    Reorganized code for clarity.  Made a random background color generator.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 // FSJS - Random Star Wars Quote Generator
 
-// Create the array of quote objects and name it quotes
+// Create the array of quote objects and name it quotes.
 
 // Assign array of quote objects to quotes variable.
+
+randomColor();
+
 var quotes = [
   { quote: "Remember…the Force will be with you, always.",
     source: "Obi Wan Kenobi",
@@ -33,35 +36,45 @@ var quotes = [
   }
 ];
 
-// Create the getRandomQuuote function and name it getRandomQuote
+// Create the getRandomQuuote function and name it getRandomQuote.
 function getRandomQuote(array) {
   // Return an a random number between 0 and 4 for use as an index for selecting a quote.
   return array[Math.floor(Math.random() * 4)];
 }
 
+// Generate a Random Background color.
+function randomColor(){
+        //Generate a random hex value between #000000 and #ffffff.
+        var hexColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = hexColor;
+      };
 
-// Create the printQuote function and name it printQuote
+
+// Create the printQuote function and name it printQuote.
 function printQuote() {
-  // Pick a quote from quotes array using previously generated random number
+  // Pick a quote from quotes array using previously generated random number.
   var selectedQuote = getRandomQuote(quotes);
   // Save the values of each key of selectedQuote by concatenating them to the outPutString var.
   var outPutString =  '<p class="quote">' + selectedQuote.quote + '</p>' +
                       '<p class="source">' + selectedQuote.source;
-  // Only concat citation if present in object
+  // Only concat citation if present in object.
   if ('citation' in selectedQuote) {
     outPutString += '<span class="citation">' + selectedQuote.citation + '</span>';
   }
-  // Only concat year if present in object
+  // Only concat year if present in object.
   if ('year' in selectedQuote) {
     outPutString += '<span class="year">' + selectedQuote.year + '</span>' +
     '</p>';
   }
   // Update Contents of HTML tags with generated quote values.
   document.getElementById('quote-box').innerHTML = outPutString;
+
+  //Change BG color
+  randomColor();
 }
 
-// This event listener will respond to "Show another quote" button clicks
-// When user clicks anywhere on the button, the "printQuote" function is called
-// Repeat with the interval of 5 seconds
+// Repeat with the interval of 5 seconds.
 var timerId = setInterval(() => printQuote(), 5000);
+// This event listener will respond to "Show another quote" button clicks.
+// When user clicks anywhere on the button, the "printQuote" function is called.
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
